05dc51f7dc3b410a9727df6ea8eb916db43f9cac
Ndurga <33654571+Ndurga@users.noreply.github.com>
2017-11-29 10:02:36 +0530

    initial upload

---
+++ common.php
@@ -0,0 +1,11 @@
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Review and C and C++ Languages">
+  <meta name="keywords" content="Reviews, C Language, C++ Language, Fresher Interview Questions">
+  <meta name="author" content="Durgaprasad N">
+  <title>Freshers School</title>
+  <link rel="stylesheet" href="/css/style.css">
+  <script src="/script/script.js" ></script>
+  <script src="/script/jquery.js" ></script>
+</head>
---
+++ errorpage.php
@@ -0,0 +1,1 @@
+<p id='errorPage'>Sorry, Page couldn't be displayed</p>
